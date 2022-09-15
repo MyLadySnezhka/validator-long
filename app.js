e7be12b4dccb25ec5b5dd2d35a84e1161bacf296
MyLadySnezhka <mylady.snezhka@gmail.com>
2022-09-15 11:42:44 +0300

    добавлены условия проверки

--- app.js
+++ app.js
@@ -22,16 +22,49 @@ server.post('/test', upload.none(), (req, res) => {
     const key1 = Object.keys(obj); 
     console.log('Первичный ключ:', Object.keys(obj)); // [ 'bio' ]
     console.log('Первичный ключ текстом:', key1[0]); // bio
+    
     const obj2 = Object.values(obj)[0]; 
     console.log('Вторичные ключи объектом:', obj2); // { work: 'ingener', status: 'busy' }
+    
     const key2 = Object.keys(obj2); 
+    console.log('Вторичные ключи массивом', key2); // [ 'work', 'status' ]
+    //или оно же будет так:
+    //console.log(Object.getOwnPropertyNames(obj2)); // [ 'work', 'status' ]
     console.log('Вторичные ключи названиями:', key2[0], key2[1]); // work status
     const val2 = Object.values(obj2);
     console.log('Значения внутренних ключей:', val2[0], val2[1]); // ingener busy
+
+    console.log('Начинаем проверку...');
+    
+    if (key1.length !== 1) {
+        console.log('Ошибка! Количество первичных полей не соответсвует заданному!');
+    } else {
+        key1.forEach((Element) => {
+            if ((Element !== 'bio')) {
+                        console.log(Element, 'Ошибка! Неправильный первичный ключ!');
+                    }
+    })};
+      
+    if (key2.length !== 2) {
+        console.log('Ошибка! Количество вторичных полей не соответсвует заданному!');
+    } else {
+        key2.forEach((Element) => {
+            //console.log(Element);
+            if ((Element !== 'work') && (Element !== 'status')) {
+                        console.log(Element, 'Ошибка! Неправильные внутренние ключи!');
+                    }
+    })};
+    
+
+    if (typeof val2[0] !== 'string') { 
+        console.log('Ошибка: поле', key2[0], 'содержит не строчное значение!'); 
+    };
+    if (typeof val2[1] !== 'string') { 
+        console.log('Ошибка: поле', key2[1], 'содержит не строчное значение!'); 
+    };
+
+    console.log('Проверка окончена!')
           
-//     if(obj.keys(data.bio).includes('work')) {
-//         console.log('ключ имя есть');
-//    };
     res.end();
 });
 
